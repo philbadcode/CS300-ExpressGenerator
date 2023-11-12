7f6abf10968eb5a8aad7f7c459087f848bf3c979
philbadcode <phillip.hail@yahoo.com>
2023-11-12 17:45:14 -0600

    added dashboard page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
+});
+
+router.post('/login', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  res.redirect('/dashboard');
 });
 
 module.exports = router;
