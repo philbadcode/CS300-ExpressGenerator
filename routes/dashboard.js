a2989d31894e54c218c5c257e7c666947b1a321f
philbadcode <phillip.hail@yahoo.com>
2023-11-12 17:54:46 -0600

    dont use node fetch

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var fetch = require('node-fetch');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET dashboard page with posts. */
 router.get('/', async function(req, res, next) {
   try {
-    // Fetch posts from jsonplaceholder
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await response.json();
+    // Fetch posts from jsonplaceholder using axios
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const posts = response.data;
 
     // Render the dashboard template with the posts data
     res.render('dashboard', { title: 'Social Media Dashboard', posts });
