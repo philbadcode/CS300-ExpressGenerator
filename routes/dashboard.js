cef43c70611fcdef6cc596579fe9bde4b75e6574
philbadcode <phillip.hail@yahoo.com>
2023-11-12 17:51:00 -0600

    updated stuff

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
 
-/* GET dashboard page. */
-router.get('/', function(req, res, next) {
-    res.render('dashboard', { title: 'Dashboard' });
-  });
+/* GET dashboard page with posts. */
+router.get('/', async function(req, res, next) {
+  try {
+    // Fetch posts from jsonplaceholder
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+
+    // Render the dashboard template with the posts data
+    res.render('dashboard', { title: 'Social Media Dashboard', posts });
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
