c8a207860f1f61d514fd2d7f838766c41dc48517
philbadcode <phillip.hail@yahoo.com>
2023-11-12 19:34:10 -0600

    added profile and navbar

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var username;
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Login' });
 });
 
 router.post('/login', function(req, res, next) {
-  username = req.body.username;
+  var username = req.body.username;
+  var password = req.body.password;
   res.redirect('/dashboard');
 });
 
